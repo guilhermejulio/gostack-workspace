8fabc43cd82d3fb8994f474e16a52222712cd065
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-01-11 22:16:17 -0300

    Create user

--- nivel-1/aula-4-typescript/src/index.ts
+++ nivel-1/aula-4-typescript/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import {helloWorld} from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
+app.get('/', helloWorld);
+
 
 app.listen(2398);
 
