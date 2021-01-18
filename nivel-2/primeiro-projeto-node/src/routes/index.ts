5d163864bddf78166333601c0ef27b2c2cdb5956
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-01-18 13:18:06 -0300

    Cadastro de agendamentos

--- nivel-2/primeiro-projeto-node/src/routes/index.ts
+++ nivel-2/primeiro-projeto-node/src/routes/index.ts
@@ -1,5 +1,8 @@
-// src/routes/index.ts
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+
 export default routes;
