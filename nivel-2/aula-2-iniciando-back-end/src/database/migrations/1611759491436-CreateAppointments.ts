ae281823325569ed9c209debdd539b2a33ce9903
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-01-27 18:24:51 -0300

    Repositório do TypeORM

--- nivel-2/aula-2-iniciando-back-end/src/database/migrations/1611759491436-CreateAppointments.ts
+++ nivel-2/aula-2-iniciando-back-end/src/database/migrations/1611759491436-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611759491436
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
