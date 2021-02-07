98eccb6a2c44932e7411f1fee20a9e5dcb51b7ea
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-07 11:19:00 -0300

    Isolando componentes

--- nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
+++ nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -11,9 +14,17 @@ const SignIn: React.FC = () => (
 
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
