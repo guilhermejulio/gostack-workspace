0b1617b22d3f51810c37e02bcd86572ae25102f7
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-07 19:28:51 -0300

    Validando cadastro

--- nivel-3/02-gobarber-web/src/pages/SignUp/index.tsx
+++ nivel-3/02-gobarber-web/src/pages/SignUp/index.tsx
@@ -1,24 +1,38 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
-
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBArber" />
+        <img src={logoImg} alt="GoBarber" />
 
         <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
