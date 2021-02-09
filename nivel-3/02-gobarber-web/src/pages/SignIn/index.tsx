1b25d28b81decc319322c9d80258ece8feec3dca
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-09 20:45:07 -0300

    API de Contexto

--- nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
+++ nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -16,6 +18,7 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { name } = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
