6bd967a15146c608b9b6e83e9523539d8a4a40dc
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-09 21:23:20 -0300

    Login pelo contexto

--- nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
+++ nivel-3/02-gobarber-web/src/pages/SignIn/index.tsx
@@ -5,7 +5,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import AuthContext from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -16,11 +16,16 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const { name } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -33,6 +38,11 @@ const SignIn: React.FC = () => {
 
       await schema.validate(data, {
         abortEarly: false,
+      });
+
+      await signIn({
+        email: data.email,
+        password: data.password,
       });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
