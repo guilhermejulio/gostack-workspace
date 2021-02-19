8aa4e2fae8a4f054bdea84e6cbe2a45bc79dbeda
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-18 22:02:34 -0300

    Camada de infra e Config. Imports

---
+++ nivel-4/01-gobarber-backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Router } from 'express';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+  // @ts-expect-error
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
