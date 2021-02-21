3987831d105a75057624705807204301e0ec3ac7
Guilherme Julio <guilhermejuliocode@gmail.com>
2021-02-21 00:38:01 -0300

    Teste de agendamento

--- nivel-4/01-gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-4/01-gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,11 +1,44 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {});
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
 });
 
-// describe('CreateAppointment', () => {
-//   it('should not be able to create two appointments on the same time', () => {
-//     expect(1 + 2).toBe(3);
-//   });
-// });
+describe('CreateAppointment', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
